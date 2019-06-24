dc98ef2456428967b2b466c6e5cbc79cfe009194
yang <1191644674@qq.com>
2019-06-24 11:15:06 +0800

    add film entity

---
+++ src/com/sansege/enuity/FilmEntity.java
@@ -0,0 +1,95 @@
+package com.sansege.enuity;
+
+public class FilmEntity {
+	private int f_id;
+	private String f_name;
+	private String f_type;
+	private String f_poster;
+	private String f_director;
+	private String f_star;
+	private String f_focus;
+	private String f_time;
+	private int f_play;
+	private String f_intro;
+	private double f_grade;
+	private int f_good;
+	private int f_bad;
+	public int getF_id() {
+		return f_id;
+	}
+	public void setF_id(int f_id) {
+		this.f_id = f_id;
+	}
+	public String getF_name() {
+		return f_name;
+	}
+	public void setF_name(String f_name) {
+		this.f_name = f_name;
+	}
+	public String getF_type() {
+		return f_type;
+	}
+	public void setF_type(String f_type) {
+		this.f_type = f_type;
+	}
+	public String getF_poster() {
+		return f_poster;
+	}
+	public void setF_poster(String f_poster) {
+		this.f_poster = f_poster;
+	}
+	public String getF_director() {
+		return f_director;
+	}
+	public void setF_director(String f_director) {
+		this.f_director = f_director;
+	}
+	public String getF_star() {
+		return f_star;
+	}
+	public void setF_star(String f_star) {
+		this.f_star = f_star;
+	}
+	public String getF_focus() {
+		return f_focus;
+	}
+	public void setF_focus(String f_focus) {
+		this.f_focus = f_focus;
+	}
+	public String getF_time() {
+		return f_time;
+	}
+	public void setF_time(String f_time) {
+		this.f_time = f_time;
+	}
+	public int getF_play() {
+		return f_play;
+	}
+	public void setF_play(int f_play) {
+		this.f_play = f_play;
+	}
+	public String getF_intro() {
+		return f_intro;
+	}
+	public void setF_intro(String f_intro) {
+		this.f_intro = f_intro;
+	}
+	public double getF_grade() {
+		return f_grade;
+	}
+	public void setF_grade(double f_grade) {
+		this.f_grade = f_grade;
+	}
+	public int getF_good() {
+		return f_good;
+	}
+	public void setF_good(int f_good) {
+		this.f_good = f_good;
+	}
+	public int getF_bad() {
+		return f_bad;
+	}
+	public void setF_bad(int f_bad) {
+		this.f_bad = f_bad;
+	}
+}
